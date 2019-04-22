341ba0e927ac2b0aeb57b48f7386a442ea3652bb
Florent Thoumie <florent@thoumie.net>
2019-04-21 19:54:46 -0700

    Update metadata for pytest / travis.
    - Fix single unittest.
    - Setup travis config.
    - Some more metadata changes.

--- api_test.py
+++ api_test.py
@@ -1,21 +1,18 @@
 import asyncio
-import unittest
-from unittest.mock import MagicMock
-
-from api import AqualinkLightToggle
+import asynctest
+from .api import AqualinkLightToggle
 
 
-def async_run(coro):
-    return asyncio.get_event_loop().run_until_complete(coro)
-
-class TestAqualinkLightToggle(unittest.TestCase):
+class TestAqualinkLightToggle(asynctest.TestCase):
     def setUp(self) -> None:
-        system = MagicMock()
+        system = asynctest.MagicMock()
+        system.set_aux = asynctest.CoroutineMock(return_value=None)
         data = {'name': 'Test Pool Light', 'state': '0', 'aux': '1'}
         self.obj = AqualinkLightToggle(system, data)
 
-    def test_switch(self) -> None:
-        async_run(self.obj.turn_off())
-        self.obj.system.mock.set_aux.assert_not_called()
-        async_run(self.obj.turn_on())
+    @asynctest.strict
+    async def test_switch(self) -> None:
+        await self.obj.turn_off()
+        self.obj.system.set_aux.assert_not_called()
+        await self.obj.turn_on()
         self.obj.system.set_aux.assert_called_once()
